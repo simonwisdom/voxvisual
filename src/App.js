7e6000563f16b71960c74da2f6984f2053f739dc
Simon Wisdom <swisdom@squareup.com>
2023-02-21 21:02:52 -0800

    added background image and improved look of waveform

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import * as THREE from "three";
-import React, { useRef } from "react";
-import { useThree } from "react-three-fiber";
-import { Canvas } from '@react-three/fiber'
+import { useRef, useState } from 'react';
+import { useThree } from 'react-three-fiber';
+import * as THREE from 'three';
+import { Canvas } from '@react-three/fiber';
+import { Text } from '@chakra-ui/react';
+
+
+function BackgroundImage({ url }) {
+  const { viewport } = useThree();
+
+  const texture = new THREE.TextureLoader().load(url);
+
+
+  const width = viewport.width;
+  const height = viewport.height;
+  const aspect = width / height;
+  return (
+    <mesh renderOrder={-1} scale={[aspect * 4, 4, 1]}>
+      <planeGeometry args={[2, 2]} />
+      <meshBasicMaterial map={texture} depthTest={false}/>
+    </mesh>
+  );
+}
 
 function VoiceVisualization() {
   const groupRef = useRef();
+  const cubeGroupRef = useRef(); // New group for cubes
   const audioRef = useRef();
   const data = new Uint8Array(128);
+  const threshold = 90; // The threshold value to trigger the animation
 
   navigator.mediaDevices
     .getUserMedia({ audio: true })
@@ -16,23 +37,47 @@ function VoiceVisualization() {
       const analyser = audioCtx.createAnalyser();
       source.connect(analyser);
 
-      const decayRate = 0.0000;
 
       function update() {
         analyser.getByteFrequencyData(data);
+
         const group = groupRef.current;
+        const cubeGroup = cubeGroupRef.current;
 
+        // Update waveform
         group.children.forEach((mesh, i) => {
-          const scale = data[i] / 200 * 10;
-          mesh.scale.y = scale;
+          mesh.scale.y = data[i] / 200;
+          // if (data[i] > threshold) {
+          //   mesh.material.color.set(0xffffff);
+          //   mesh.material.emissive.set(0xffffff);
+          // } else {
+          //   mesh.material.color.set(0x000000);
+          //   mesh.material.emissive.set(0x000000);
+          // }
+        });
 
-          // Move the mesh down the screen
-          mesh.position.y -= decayRate * (1 + scale);
+        // console.log(data);
 
-          // Fade out the mesh as it moves down the screen
-          const material = mesh.material;
-          const opacity = material.opacity - decayRate / 2;
-          material.opacity = opacity < 0 ? 0 : opacity;
+
+        // Trigger animation for cubes
+        cubeGroup.children.forEach((cube, i) => {
+          // const distance = Math.abs(data[i * 4] - 128);
+          if (data[i*8] > threshold) {
+            cube.material.color.set(0xffffff);
+            cube.material.emissive.set(0xffffff);
+          } else {
+            cube.material.color.set(0x000000);
+            cube.material.emissive.set(0x000000);
+          }
+
+
+
+          // console.log(distance, data[i * 4]);
+          // if (distance > threshold) {
+          // cube.userData.triggered = true;
+          // cube.material.color.setRGB(1, 1, 1);
+            // cube.material.emissive.set('blue');
+          // }
         });
       }
 
@@ -44,28 +89,47 @@ function VoiceVisualization() {
       render();
     });
 
-  const items = [];
-  for (let i = 0; i < 128; i++) {
-    const item = (
-      <mesh key={i} position={[(i - 64) / 4, 10, -20]}>
-        <boxBufferGeometry args={[0.2, 0.2, 0.2]} />
-        <meshStandardMaterial color={new THREE.Color().setHSL(i / 128, 1, 0.5)} />
-      </mesh>
-    );
-    items.push(item);
-  }
+ // Create cubes
+ const cubes = [];
+ for (let i = 0; i < 16; i++) {
+   const cube = (
+     <mesh key={i} position={[(i - 16) * 2, -5, -20]} userData={{ triggered: false }}>
+       <boxBufferGeometry args={[1, 1, 1]} />
+       <meshStandardMaterial color={'red'} />
+     </mesh>
+   );
+   cubes.push(cube);
+ }
 
-  return <group ref={groupRef}>{items}</group>
+ return (
+   <>
+     <group ref={groupRef}>
+       {[...Array(128)].map((_, i) => (
+         <mesh key={i} position={[15 + (i - 64) / 3, 8, -45 ]}>
+           <boxBufferGeometry args={[0.1, 10, 0.2]} />
+           <meshStandardMaterial color={new THREE.Color().setHSL(i / 128, 1, 0.5)} />
+         </mesh>
+       ))}
+     </group>
+     <group ref={cubeGroupRef} position={[10, -15, -20]}>
+       {cubes}
+     </group>
+   </>
+ );
 }
+
 
 
 function App() {
   return (
     <div style={{ height: '100vh' }}>
     <Canvas>
+    <BackgroundImage url={require('./assets/sophia-desk-blade-runner.png')} />
+
       <VoiceVisualization />
       <ambientLight />
     </Canvas>
+
     </div>
   );
 }
